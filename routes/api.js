91e1f36ea08cc945130ad98a7866c36cc5de3f02
Dylan <changliangdylan@gmail.com>
2023-10-17 20:20:18 -0400

    adding database mongoose

--- routes/api.js
+++ routes/api.js
@@ -2,31 +2,31 @@ const express = require('express');
 const router = express.Router();
 
 // GET all Products
-router.get('/api/products', (req, res) => {
+router.get('/products', (req, res) => {
 });
 
 // GET Product by id
-router.get('/api/products/:id', (req, res) => {
+router.get('/products/:id', (req, res) => {
 });
 
 // POST new Product
-router.post('/api/products', (req, res) => {
+router.post('/products', (req, res) => {
 });
 
 // PUT update Product by id
-router.put('/api/products/:id', (req, res) => {
+router.put('/products/:id', (req, res) => {
 });
 
 // DELETE remove Product by id
-router.delete('/api/products/:id', (req, res) => {
+router.delete('/products/:id', (req, res) => {
 });
 
 // DELETE remove all Products
-router.delete('/api/products', (req, res) => {
+router.delete('/products', (req, res) => {
 });
 
 // GET find all Products which name contains 'kw'
-router.get('/api/products', (req, res) => {
+router.get('/products', (req, res) => {
   const { name } = req.query;
 });
 
